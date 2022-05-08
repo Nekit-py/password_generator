4c5cff992376360c1e63ccd17609cd4a7674be02
Nekit-py <nekit-sns@yandex.ru>
2022-05-08 22:51:22 +0700

    Completed

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,6 @@
-use clap::{App, Arg, ArgMatches};
 use rand::seq::SliceRandom;
-use rand::{thread_rng, Rng};
-use std::array;
-use std::error::Error;
-use std::process::Command;
-
-
-// fn set_comandline_args<'a>() -> ArgMatches<'a>{
-//    let matches = App::new("Password generator")
-//        .version("0.1.0")
-//        .author("Nekit S. <nekit-sns@yandex.ru>")
-//        .about("Generates password for your needs.")
-//        .arg(Arg::with_name("lenght")
-//                 .short("l")
-//                 .long("lenght")
-//                 .takes_value(true)
-//                 .help("Takes password lenght."))
-//        .arg(Arg::with_name("Password difficulty.")
-//                 .short("d")
-//                 .long("difficulty")
-//                 .takes_value(true)
-//                 .help("Takes password difficulty (optional)."))
-//        .get_matches();
-//    matches
-// }
-
+use rand::thread_rng;
+use std::io;
 
 const SYMBOLS: [char; 19] = [
     '!', '@', '#', '$', '%', '?', '*', '-', '_', '=', '+', ':', ';', '.', ',', '(', ')', '|', '/',
@@ -32,18 +8,15 @@ const SYMBOLS: [char; 19] = [
 
 const NUMBERS: [char; 10] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-
 const LETTERS_LOWER: [char; 26] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-
 const LETTERS_UPPER: [char; 26] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
-
 
 struct PasswordProperties {
     len: u8,
@@ -54,20 +27,27 @@ impl Default for PasswordProperties {
     fn default() -> Self {
         PasswordProperties {
             len: 8,
-            difficulty: vec![LETTERS_LOWER.to_vec(), LETTERS_UPPER.to_vec(), NUMBERS.to_vec()],
+            difficulty: vec![
+                LETTERS_LOWER.to_vec(),
+                LETTERS_UPPER.to_vec(),
+                NUMBERS.to_vec(),
+            ],
         }
     }
 }
 
 impl PasswordProperties {
     fn new(len: Option<u8>, difficulty: Option<Vec<Vec<char>>>) -> PasswordProperties {
-        PasswordProperties  {
+        PasswordProperties {
             len: len.unwrap_or(8),
-            difficulty: difficulty.unwrap_or(vec![LETTERS_LOWER.to_vec(), LETTERS_UPPER.to_vec(), NUMBERS.to_vec()]),
+            difficulty: difficulty.unwrap_or(vec![
+                LETTERS_LOWER.to_vec(),
+                LETTERS_UPPER.to_vec(),
+                NUMBERS.to_vec(),
+            ]),
         }
     }
 }
-
 
 struct Password {
     password: String,
@@ -88,15 +68,12 @@ impl Default for Password {
             pwd.push(**pwd_chars.choose(&mut thread_rng()).unwrap());
         }
 
-        Password {
-            password: pwd
-        }
+        Password { password: pwd }
     }
 }
 
 impl Password {
     fn gen_pwd(properties: PasswordProperties) -> Password {
-        let properties = properties;
         let mut pwd = String::new();
         let mut pwd_chars = Vec::new();
 
@@ -109,32 +86,41 @@ impl Password {
             pwd.push(**pwd_chars.choose(&mut thread_rng()).unwrap());
         }
 
-        Password {
-            password: pwd
-        }
+        Password { password: pwd }
     }
 }
 
+fn get_user_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line.");
+    input
+}
 
 fn main() {
-    let pwd_default = Password::default();
-    let pwd_easy = {
-        let properties = PasswordProperties::new(
-            Some(12),
-            Some(vec![NUMBERS.to_vec(), LETTERS_LOWER.to_vec()])
-        );
-        Password::gen_pwd(properties)
+    println!("Please set the password length:");
+    let length = {
+        let input_string = get_user_input();
+        input_string.trim().parse::<u8>().unwrap()
     };
 
-    let pwd_hard = {
-        let properties = PasswordProperties::new(
-            Some(12),
-            Some(vec![LETTERS_LOWER.to_vec(), LETTERS_UPPER.to_vec(), NUMBERS.to_vec(), SYMBOLS.to_vec()])
-        );
-        Password::gen_pwd(properties)
+    println!("Please set the password difficulty:");
+    let difficulty = {
+        match get_user_input().as_str() {
+            "easy" | "simple" | "low" => Some(vec![NUMBERS.to_vec(), LETTERS_LOWER.to_vec()]),
+            "medium" | "default" | "midle" => None,
+            "hard" | "strong" | "expert" => Some(vec![
+                LETTERS_LOWER.to_vec(),
+                LETTERS_UPPER.to_vec(),
+                NUMBERS.to_vec(),
+                SYMBOLS.to_vec(),
+            ]),
+            _ => None,
+        }
     };
 
-    println!("{:?}", pwd_easy.password);
-    println!("{:?}", pwd_default.password);
-    println!("{:?}", pwd_hard.password);
+    let properties = PasswordProperties::new(Some(length), difficulty);
+    let password = Password::gen_pwd(properties);
+    println!("New password is -> {}", password.password);
 }
